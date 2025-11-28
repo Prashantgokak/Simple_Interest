049659f95dd7407d5eaa46517ad6e2fbd2b63ee2
Prashantgokak <prashantgokak500@gmail.com>
2025-11-28 10:26:04 +0530

    save

--- simpleinterest.py
+++ simpleinterest.py
@@ -1,11 +1,13 @@
 import sys
+
+
 if len(sys.argv) == 4:
     script_name = sys.argv[0]
     principle = float(sys.argv[1])
     rate = float(sys.argv[2])
     time = float(sys.argv[3])
 else:
-
+    
     script_name = sys.argv[0]
     principle = 1000.0
     rate = 5.0
